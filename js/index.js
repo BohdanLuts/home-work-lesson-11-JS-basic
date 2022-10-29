8509cfed2f8fde53a1378de85b6229ffebafb773
Bohdan <theodoronos@gmail.com>
2022-10-29 23:00:14 +0300

    Add completed task 1

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// variables task 1
+let a = 2;
+let b = 3;
+let multiplication = a * b;
+console.log("1. a * b =", multiplication);
+
+// variables task 2
+let c = 8;
+let d = 4;
+let result = c / d;
+console.log("2. c / d =", result);
+
+// variables task 3
+let e = 3;
+let f = 9.5;
+let summ = e + f;
+console.log("3. e + f =", summ);
+
+// variables task 4
+let number = 11;
+console.log("4.1.", number);
+
+let boolean = true;
+console.log("4.2.", boolean);
+
+let string = '"java script"';
+console.log("4.3.", string);
+
+let textNumber = '"100"';
+console.log("4.4.", textNumber);
+
+// variables task 5
+let num = 1;
+num += 11; // num = num + 11;
+console.log("5.1", num);
+
+num -= 11; // num = num – 11;
+console.log("5.2", num);
+
+num *= 11; // num = num * 11;
+console.log("5.4.", num);
+
+num /= 11; // num = num / 11;
+console.log("5.5", num);
+
+++num; // num = num + 1;
+console.log("5.6", num);
+
+--num; // num = num – 1;
+console.log("5.7", num);
